c0af9a32938f6b32adaac41b68d2335e6556e55c
Overraddit <kenwooshim@gmail.com>
2021-05-09 01:53:56 +0900

    publish

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const {WebhookClient} = require('dialogflow-fulfillment');
+
+const app = express()
+app.use(bodyParser.json())
+const port = process.env.PORT || 3000
+
+app.post('/dialogflow-fulfillment', (request, response) => {
+    console.log("test")
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
+
+const dialogflowFulfillment = (request, response) => {
+    const agent = new WebhookClient({request, response})
+
+    function sayHello(agent){
+        agent.add("Hi there, this response is coming from heroku")
+    }
+
+    // 인텐트와 함수를 1대1 대응 시키는 객체 intentMap
+    let intentMap = new Map();
+    //intentMap.set("Default Welcome Intent", sayHello)
+    intentMap.set("Lecture", sayHello)
+}
