44e358eb211f839db200498e8266a7f2d4b58dad
Overraddit <kenwooshim@gmail.com>
2021-05-09 02:03:25 +0900

    last

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ app.use(bodyParser.json())
 const port = process.env.PORT || 3000
 
 app.post('/dialogflow-fulfillment', (request, response) => {
-    console.log("test")
+    dialogflowFulfillment(request, response)
 })
 
 app.listen(port, () => {
-    dialogflowFulfillment(request, response)
+    console.log(`Listening on port ${port}`)
 })
 
 const dialogflowFulfillment = (request, response) => {
@@ -23,7 +23,7 @@ const dialogflowFulfillment = (request, response) => {
 
     // 인텐트와 함수를 1대1 대응 시키는 객체 intentMap
     let intentMap = new Map();
-    intentMap.set("Default Welcome Intent", sayHello)
-    //intentMap.set("Lecture", sayHello)
+    //intentMap.set("Default Welcome Intent", sayHello)
+    intentMap.set("Lecture", sayHello)
     agent.handleRequest(intentMap);
 }
