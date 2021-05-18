e0cdaa25eaa702d8468c8de50e2f992208e63d9d
Overraddit <kenwooshim@gmail.com>
2021-05-19 00:28:54 +0900

    plus sayName

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 3000
 
 app.post('/dialogflow-fulfillment', (request, response) => {
     dialogflowFulfillment(request, response)
+    
 })
 
 app.listen(port, () => {
@@ -21,9 +22,15 @@ const dialogflowFulfillment = (request, response) => {
         agent.add("안녕안녕안녕안녕~~")
     }
 
+    function sayName(agent){
+        var name = req.body.queryResult.parameters['name']
+        agent.add(`[heroku]${name}님, 안녕하세요!`)
+    }
+
     // 인텐트와 함수를 1대1 대응 시키는 객체 intentMap
     let intentMap = new Map();
     intentMap.set("Default Welcome Intent", sayHello)
     intentMap.set("Lecture", sayHello)
+    intentMap.set("askEmail", sayName)
     agent.handleRequest(intentMap);
 }
