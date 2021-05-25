95ffaf1a37d0167463e247252a770017542c4181
Overraddit <kenwooshim@gmail.com>
2021-05-25 23:37:24 +0900

    weathertest6

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+var axios = require('axios')
 const {WebhookClient} = require('dialogflow-fulfillment');
+const { request } = require('express');
 
 const app = express()
 app.use(bodyParser.json())
