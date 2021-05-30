2de15a4d93b5d6cd25b2e90bfd16a57b2e22ce54
Overraddit <kenwooshim@gmail.com>
2021-05-30 23:22:10 +0900

    asdfasdf

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ const app = express()
 app.use(bodyParser.json())
 const port = process.env.PORT || 3000
 const openweatherappid = "aca3d57df145ee10c372ff22aefdaa56";
+const city;
+
 app.post('/dialogflow-fulfillment', (request, response) => {
     dialogflowFulfillment(request, response)
-    
+    city = req.body.queryResult.parameters['geo-city']
 })
 
 app.listen(port, () => {
@@ -28,55 +30,33 @@ const dialogflowFulfillment = (request, response) => {
         var name = agent.request_.body.queryResult.outputContexts[0].parameters['name.original'];
         agent.add("[heroku]아하, 당신의 이름은 <" + name + "> 군요!!");
     }
-
-    function sayWeather(agent){
-
-        // 1번째 시도
-        var myurl = "http://api.openweathermap.org/data/2.5/weather?q=seoul&appid=aca3d57df145ee10c372ff22aefdaa56";
-        agent.add("start of function");
-            $.ajax({
-                url : myurl,
-                method : 'GET',
-                success :  (data)=> {
-                    // 여기를 주목
-                    console.log("======================first======================")
-                    console.log(data);
-                    console.log(data.main);
-                    console.log(data.main.temp);
-                    console.log("============================================")
-                    var temp = String((data.main.temp - 272)).substring(0,3); // 온도
-                    var location = data.name; // 지역이름 
-                    agent.add('지역 ：' + location + ' 온도　：' + temp　+ "도입니다. "+'\n');
-                  }
-            });
-        agent.add("end of function");
-    }
     
     function helloWorld() {
         // 2번째시도 axios => DEADLINE EXCEED error
         return axios({
           method: "GET",
-          url: "http://api.openweathermap.org/data/2.5/weather?q=seoul&appid=aca3d57df145ee10c372ff22aefdaa56",
+          url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${openweatherappid}`,
           data: "",
         })
           .then((response) => {
             console.log("======================second======================")
+            console.log(url);
             console.log(response.data.main.temp - 272); //Hello World
             var temperature = String(response.data.main.temp - 272)
             console.log("============================================")
-            agent.add("오늘 서울의 날씨는 현재 섭씨"+ temperature + "입니다 !"); 
+            agent.add("오늘 " + city + "은(는) 현재 섭씨"+ temperature + "입니다 !"); 
           })
           .catch((error) => {
             console.log(error);
           });
         
         
-        return axios.get('http://api.openweathermap.org/data/2.5/weather?q=seoul&appid=aca3d57df145ee10c372ff22aefdaa56').then((Response)=>{
-          console.log(response.data.main.temp - 272); //Hello World
-          agent.add(response.data.main.temp - 272); 
-        }).catch((Error)=>{
-            console.log(Error);
-        });
+        // return axios.get('http://api.openweathermap.org/data/2.5/weather?q=seoul&appid=aca3d57df145ee10c372ff22aefdaa56').then((Response)=>{
+        //   console.log(response.data.main.temp - 272); //Hello World
+        //   agent.add(response.data.main.temp - 272); 
+        // }).catch((Error)=>{
+        //     console.log(Error);
+        // });
         
     }
       
