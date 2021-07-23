3b3c817217d8d8b4c168bff7bcc85bd00a94a293
Overraddit <kenwooshim@gmail.com>
2021-07-23 22:42:33 +0900

    YAHO

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const dialogflowFulfillment = (request, response) => {
         var name = agent.request_.body.queryResult.outputContexts[0].parameters['name.original'];
         agent.add("[heroku]아하, 당신의 이름은 <" + name + "> 군요!!");
     }
-    
+
     function helloWorld() {
         // 2번째시도 axios => DEADLINE EXCEED error
         //var city = agent.request_.body.queryResult.outputContexts[0].parameters['location.original']['city'];
@@ -44,23 +44,26 @@ const dialogflowFulfillment = (request, response) => {
         return axios({
           method: "GET",
           //api.openweathermap.org/data/2.5/forecast?q={city name}&appid
-          url: encodeURI("http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=aca3d57df145ee10c372ff22aefdaa56"),
+          //url: encodeURI("http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=aca3d57df145ee10c372ff22aefdaa56"),
+          url: encodeURI("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=aca3d57df145ee10c372ff22aefdaa56"),
           data: "",
         })
           .then((response) => {
             console.log("======================second======================")
             console.log(response)
+            console.log("============================================")
             console.log(response.data)
+            console.log("============================================")
             console.log(response.data.main.temp - 272); //Hello World
             var temperature = String(response.data.main.temp - 272)
             console.log("============================================")
-            agent.add(original_date + city + "은(는) 현재 섭씨"+ temperature + "입니다 !"); 
+            agent.add(original_date + city + "은(는) 현재 섭씨"+ temperature + "입니다 !");
           })
           .catch((error) => {
             console.log(error);
-          });        
+          });
     }
-      
+
 
     // 인텐트와 함수를 1대1 대응 시키는 객체 intentMap
     let intentMap = new Map();
